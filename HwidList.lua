a94b1b14e5186ee18041bfcded455a2e61fdce6e
Amazonek123 <niepoemem741@gmail.com>
2025-03-02 19:11:01 +0100

    Added new player via Roblox

--- HwidList.lua
+++ HwidList.lua
@@ -1,6 +1,7 @@
 return {
     ["Player1"] = "HWID-123456",
-    ["Player3"] = "HWID-789012",
+    ["BCI6dWVyNGR8NGN8NGN8"] = "DCCF86C4-93E3-4CCE-8ECE-4D73CC8FB6CE",
     ["Player2"] = "HWID-654321",
+    ["Player3"] = "HWID-789012",
     ["Player2632"] = "HWID-210738",
 }
