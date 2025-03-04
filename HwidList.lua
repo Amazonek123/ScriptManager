5ba440412affd042d4fbbbb62b004fd00388cdda
Amazonek123 <niepoemem741@gmail.com>
2025-03-04 20:15:26 +0100

    Update HwidList.lua

--- HwidList.lua
+++ HwidList.lua
@@ -1,3 +1,3 @@
 return {
-    ["pqqup320qP^2p3uoqqqt^^yur^r0^aqtp^@tp^iq@qg#"] = "DCCF86C4-93E3-4CCE-8ECE-4D73CC8FB6CA",
+    ["pqqup320qP^2p3uoqqqt^^yur^r0^aqtp^@tp^iq@qg#"] = "DCCF86C4-93E3-4CCE-8ECE-4D73CC8FB6CE",
 }
